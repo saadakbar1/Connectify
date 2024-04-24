b8154b6ac698e6140991832801533aea3720d82f
Saad Akbar <102979976+saadakbar1@users.noreply.github.com>
2024-04-25 02:31:57 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,69 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      
+      },
+    },
+  
+
+    extend: {
+      colors: {
+        'primary-500': '#FF0000',
+        'primary-600': '#D21312',
+        'secondary-500': '#FFB620',
+        'off-white': '#D0DFFF',
+        'red': '#FF5A5A',
+        'dark-1': '#000000',
+        'dark-2': '#09090A',
+        'dark-3': '#101012',
+        'dark-4': '#1F1F22',
+        'light-1': '#FFFFFF',
+        'light-2': '#EFEFEF',
+        'light-3': '#F15A59',
+        'light-4': '#5C5C7B',
+        'light-5' : '#CDCDCA'
+      },
+      screens: {
+        'xs': '480px',
+      
+      },
+      width: {
+        '420': '420px',
+        '465': '465px',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
+};
